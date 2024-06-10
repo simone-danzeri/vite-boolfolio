aa0cc021e61117b4924e2f2f100ad43d078e9e42
simone-danzeri <s.danzeri@icloud.com>
2024-06-10 14:17:05 +0200

    feat: milestone 5: created component for single ProjectCard

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,39 @@
+<script>
+    export default {
+        name: 'ProjectCard',
+        props: {
+            project: Object
+        }
+    }
+</script>
+
+<template>
+        <!-- Single card -->
+        <div class="col my-2">
+            <div class="card">
+                <!-- <img src="..." class="card-img-top" alt="..."> -->
+                <div class="card-body">
+                    <h3 class="card-title">{{ project.name }}</h3>
+                    <p class="card-text my-2">
+                        <strong>Client Name</strong>: <span class="fw-light">{{ project.client_name }}</span>
+                    </p>
+                    <div v-if="project.type" class="card-text my-2">
+                        <strong>Project Type</strong>: <span class="fw-light">{{ project.type.name }}</span>
+                    </div>
+                    <div v-if="project.technologies.length > 0" class="card-text my-2">
+                        <strong>Technologies</strong>: <span v-for="techs in project.technologies" class="fw-light">{{ techs.name }}&nbsp;</span>
+                    </div>
+                    <p v-if="project.summary" class="card-text my-2">
+                        <strong>Project Summary</strong>: <span class="fw-lighter">{{ project.summary }}</span>
+                    </p>
+                    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                </div>
+            </div>
+        </div>
+        <!-- /Single card -->
+</template>
+
+
+<style scoped lang="scss">
+
+</style>
--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -1,7 +1,11 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
     export default {
         name: 'ProjectsList',
+        components: {
+            ProjectCard,
+        },
         data() {
             return {
                 projects : [],
@@ -27,29 +31,7 @@ import axios from 'axios';
 <div class="container">
     <h1 class="text-center mt-2 mb-4">Projects List</h1>
     <div class="row row-cols-3">
-        <!-- Single card -->
-        <div class="col my-2" v-for="project in projects">
-            <div class="card">
-                <!-- <img src="..." class="card-img-top" alt="..."> -->
-                <div class="card-body">
-                    <h3 class="card-title">{{ project.name }}</h3>
-                    <p class="card-text my-2">
-                        <strong>Client Name</strong>: <span class="fw-light">{{ project.client_name }}</span>
-                    </p>
-                    <div v-if="project.type" class="card-text my-2">
-                        <strong>Project Type</strong>: <span class="fw-light">{{ project.type.name }}</span>
-                    </div>
-                    <div v-if="project.technologies.length > 0" class="card-text my-2">
-                        <strong>Technologies</strong>: <span v-for="techs in project.technologies" class="fw-light">{{ techs.name }}&nbsp;</span>
-                    </div>
-                    <p v-if="project.summary" class="card-text my-2">
-                        <strong>Project Summary</strong>: <span class="fw-lighter">{{ project.summary }}</span>
-                    </p>
-                    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-                </div>
-            </div>
-        </div>
-        <!-- /Single card -->
+        <ProjectCard v-for="project in projects" :project="project"></ProjectCard>
     </div>
 </div>
 </template>
