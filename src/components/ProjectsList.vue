bd40de564769b0d26c4b597b9480404a5f87636a
simone-danzeri <s.danzeri@icloud.com>
2024-06-10 14:08:10 +0200

    feat: milestone 4: printed all projects

--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -27,16 +27,29 @@ import axios from 'axios';
 <div class="container">
     <h1 class="text-center mt-2 mb-4">Projects List</h1>
     <div class="row row-cols-3">
-        <div class="col">
+        <!-- Single card -->
+        <div class="col my-2" v-for="project in projects">
             <div class="card">
                 <!-- <img src="..." class="card-img-top" alt="..."> -->
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h3 class="card-title">{{ project.name }}</h3>
+                    <p class="card-text my-2">
+                        <strong>Client Name</strong>: <span class="fw-light">{{ project.client_name }}</span>
+                    </p>
+                    <div v-if="project.type" class="card-text my-2">
+                        <strong>Project Type</strong>: <span class="fw-light">{{ project.type.name }}</span>
+                    </div>
+                    <div v-if="project.technologies.length > 0" class="card-text my-2">
+                        <strong>Technologies</strong>: <span v-for="techs in project.technologies" class="fw-light">{{ techs.name }}&nbsp;</span>
+                    </div>
+                    <p v-if="project.summary" class="card-text my-2">
+                        <strong>Project Summary</strong>: <span class="fw-lighter">{{ project.summary }}</span>
+                    </p>
                     <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
                 </div>
             </div>
         </div>
+        <!-- /Single card -->
     </div>
 </div>
 </template>
